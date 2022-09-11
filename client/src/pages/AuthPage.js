e4723ff5ac3706cdda434dfa3ce74a12f936229e
Lex <fischer84@mail.ru>
2022-09-11 09:06:31 +0300

    started client

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthPage = () => {
+    return (
+        <div>
+            Autorization
+        </div>
+    );
+};
+
+export default AuthPage;
---
+++ client/src/routes.js
@@ -0,0 +1,31 @@
+import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE, SHOP_ROUTE } from "./utils/consts";
+
+export const authRoutes = [
+    {
+        path: ADMIN_ROUTE,
+        Component: AdminPage
+    },
+    {
+        path: BASKET_ROUTE,
+        Component: BasketPage
+    }
+];
+
+export const publicRoutes = [
+    {
+        path: SHOP_ROUTE,
+        Component: ShopPage
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: AuthPage
+    },
+    {
+        path: REGISTER_ROUTE,
+        Component: AuthPage
+    },
+    {
+        path: DEVICE_ROUTE + '/:id',
+        Component: DevicePage
+    }
+];
